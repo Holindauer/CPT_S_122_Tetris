a73dccc80876767e7f1ba0c28c7da5430984f604
JeffLepp <jeffersonkline@gmail.com>
2024-04-14 14:11:25 -0700

    added basic graphics, push demo from Jeff

--- gameLoop.cpp
+++ gameLoop.cpp
@@ -9,10 +9,22 @@ void gameLoop(){
     Board board;
 	board.newTetrisPiece();
 
-    sf::RenderWindow window(sf::VideoMode(3000, 1000), "Tetris!");
+    sf::RenderWindow window(sf::VideoMode(320, 800), "Tetris!");
+
+	sf::RectangleShape border(sf::Vector2f(240, 720));
+	border.setOutlineThickness(5);
+    border.setPosition(40, 40);
+    border.setFillColor(sf::Color::Transparent);
+	border.setOutlineColor(sf::Color::Blue);
+
+
 
 	while (window.isOpen())
 	{
+
+		window.clear();
+		window.draw(border);
+		window.display();
 
 		sf::Event event;
 		while (window.pollEvent(event))
@@ -21,18 +33,18 @@ void gameLoop(){
 				window.close();
 		}
 
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
+		// if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
 
-			// update representation in memory
-			board.moveRight();
-		}
+		// 	// update representation in memory
+		// 	board.moveRight();
+		// }
 
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
+		// if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
 
-			// update representation in memory
-			board.moveLeft();
+		// 	// update representation in memory
+		// 	board.moveLeft();
 
-		}
+		// }
 
         // system("clear");
         // board.printBoard();
